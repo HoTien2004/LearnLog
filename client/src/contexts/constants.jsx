2911579223b088b0b5eb048bfcfdb17c97b18535
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-06-10 23:09:56 +0700

    17. Update apiUrl to use the correct production endpoint for deployment

--- client/src/contexts/constants.jsx
+++ client/src/contexts/constants.jsx
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://your-production-url.com/api';
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://tienho-learnlog.onrender.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learn_log';
 
